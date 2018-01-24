b28e6320f2697af517c7b569dcd9a49514cd0006
Shameera Rakiep <shameerarakiep@gmail.com>
2018-01-24 12:37:12 +0200

    ajax call results are now being pushed into an array for easy searching

--- map-project.js
+++ map-project.js
@@ -1,10 +1,10 @@
-var searchBox = document.querySelector(".searchBox").value;
-var searchResult = [];
+
 
 
 
     $(document).ready(function(){
-
+        var searchBox = document.querySelector(".searchBox").value;
+        var searchResult = [];
     //search from searchbox needs run through before the ajax call is made
 
         $.ajax({
@@ -12,11 +12,27 @@ type: "GET",
 url: "https://developer.nrel.gov/api/windexchange/schoolprojects?api_key=BpwET3I8qcPGHgBcgcECMNuYXfDVEz3zwKN00w1f",
 dataType: "json",
 success: function(results){
-    searchResult.push(results.value);
+    console.log(results)
+    for (var i=0; i<results.length; i++){
+        searchResult.push(results[i].City.trim());  
+          
+    }
+    
 }    
 }); //end of ajax call for data from education and training api
 
 console.log(searchResult);
+
+var searchBtn = document.querySelector(".searchBtn");
+
+searchBtn.addEventListener("click", function searchPlace (){
+    for (var i=0; i<searchResult[0].length; i++){
+        if (searchBox === searchResult[i]){
+            console.log(searchResult[i])
+        }
+    }
+})
+
 
 function myMap() {
     var mapProp= {
