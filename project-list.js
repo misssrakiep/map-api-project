241b97a7547d17f361d9778e73896a0510918ff5
Shameera Rakiep <shameerarakiep@gmail.com>
2018-01-26 10:13:38 +0200

    working on page layout

--- project-list.js
+++ project-list.js
@@ -9,39 +9,42 @@ $(document).ready(function(){
         url: "https://developer.nrel.gov/api/windexchange/schoolprojects?api_key=BpwET3I8qcPGHgBcgcECMNuYXfDVEz3zwKN00w1f",
         dataType: "json",
         success: function(results){
-            console.log(results)
-            for (var i=0; i<results.length; i++){
-                searchResult.push({
-                    City: results[i].City.trim(), 
-                    Latitude: results[i].Latitude, 
-                    Longitude: results[i].Longitude,
-                    Projects: results[i].ProjectName.trim(),
-                    Address: results[i].Address,
-                    Country: results[i].CountryName.trim(),
-                    ProjectType: results[i].WfSType
-                });  
+            console.log(results);
+              document.querySelector('.projectCard').innerHTML = projectTemp({
+                results: results
+                });
+            // for (var i=0; i<results.length; i++){
+            //     searchResult.push({
+            //         City: results[i].City.trim(), 
+            //         Latitude: results[i].Latitude, 
+            //         Longitude: results[i].Longitude,
+            //         Projects: results[i].ProjectName.trim(),
+            //         Address: results[i].Address,
+            //         Country: results[i].CountryName.trim(),
+            //         ProjectType: results[i].WfSType
+            //     });  
                 
-            }
+            // }
               
         }
     }); //end of ajax call for data from education and training api
     
-    function allProjects() {
-        console.log("------");
-        console.log(searchResult); 
-            for (var i=0; i<searchResult.length; i++){
-                document.querySelector('.projectCard').innerHTML = projectTemp({
-                    city : searchResult.City,
-                    project : searchResult.Projects,
-                    projectType: searchResult.ProjectType,
-                    address : searchResult.Address,
-                    country : searchResult.Country
-                });
-            }
+    // function allProjects() {
+    //     console.log("------");
+    //     console.log(searchResult); 
+    //         for (var i=0; i<searchResult.length; i++){
+    //             document.querySelector('.projectCard').innerHTML = projectTemp({
+    //                 city : searchResult.City,
+    //                 project : searchResult.Projects,
+    //                 projectType: searchResult.ProjectType,
+    //                 address : searchResult.Address,
+    //                 country : searchResult.Country
+    //             });
+    //         }
 
 
         
-    }
+    // }
 
-    allProjects();
+    // allProjects();
 }); //End of document ready
