d3ab9f9f8f94cab7ec37f2c206c9f508bb2cfe08
Shameera Rakiep <shameerarakiep@gmail.com>
2018-01-26 11:28:55 +0200

    header and footer for the list of projects file

--- project-list.js
+++ project-list.js
@@ -12,39 +12,7 @@ $(document).ready(function(){
             console.log(results);
               document.querySelector('.projectCard').innerHTML = projectTemp({
                 results: results
-                });
-            // for (var i=0; i<results.length; i++){
-            //     searchResult.push({
-            //         City: results[i].City.trim(), 
-            //         Latitude: results[i].Latitude, 
-            //         Longitude: results[i].Longitude,
-            //         Projects: results[i].ProjectName.trim(),
-            //         Address: results[i].Address,
-            //         Country: results[i].CountryName.trim(),
-            //         ProjectType: results[i].WfSType
-            //     });  
-                
-            // }
-              
+                });   
         }
     }); //end of ajax call for data from education and training api
-    
-    // function allProjects() {
-    //     console.log("------");
-    //     console.log(searchResult); 
-    //         for (var i=0; i<searchResult.length; i++){
-    //             document.querySelector('.projectCard').innerHTML = projectTemp({
-    //                 city : searchResult.City,
-    //                 project : searchResult.Projects,
-    //                 projectType: searchResult.ProjectType,
-    //                 address : searchResult.Address,
-    //                 country : searchResult.Country
-    //             });
-    //         }
-
-
-        
-    // }
-
-    // allProjects();
 }); //End of document ready
